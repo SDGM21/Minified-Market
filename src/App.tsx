19f4039b6dec663600528989d2bfdc8486f7911c
Santiago David <santiagodaviduno@hotmail.com>
2022-09-30 17:20:30 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ const App = () => {
   return (
   <div className="container">
     <h1>NO</h1>
-    <div style={{color:"black", border:"5px solid", display:"inline-block"}}>TESTING</div>
+    <div style={{color:"black", border:"5px solid", display:"inline-block"}}>TESTING 2</div>
   </div>);
 }
 
