f3d92260b4856a077949bc902bfa7cc8bcc58f9a
Nick Flores <nick.flores@whiz.pe>
2022-10-02 00:15:14 -0500

    banner text added

--- src/components/home/Banner.tsx
+++ src/components/home/Banner.tsx
@@ -5,8 +5,8 @@ export const HeaderBanner = () => {
 	return (<>
 		<section id="header-banner">
 			<Container>
-				<Row className="d-flex justify-center">
-					<Col>
+				<Row className="d-flex justify-center text-center">
+					<Col xs={12}>
 						<nav>
 							<ul className="d-flex">
 								<li><Link to=""></Link></li>
@@ -16,15 +16,13 @@ export const HeaderBanner = () => {
 							</ul>
 						</nav>
 					</Col>
-				</Row>
-				<Row >
-					<Col>
+					<Col xs={12}>
 						<h1>Explore the world’s leading design portfolios</h1>
 						<span>Millions of designers and agencies around the world showcase their portfolio work on Dribbble - the home to the world’s best design and creative professionals.</span>
 					</Col>
-				</Row>
-				<Row>
-
+					<Col xs={12} className="my-5">
+						<input type="text" />
+					</Col>
 				</Row>
 			</Container>
 		</section>
