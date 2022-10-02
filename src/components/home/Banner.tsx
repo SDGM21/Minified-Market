51b80a14c2b580742d58014a5fe81415692dcfb1
Nick Flores <nick.flores@whiz.pe>
2022-10-02 00:20:17 -0500

    adding navbar banner items

--- src/components/home/Banner.tsx
+++ src/components/home/Banner.tsx
@@ -5,19 +5,22 @@ export const HeaderBanner = () => {
 	return (<>
 		<section id="header-banner">
 			<Container>
-				<Row className="d-flex justify-center text-center">
+				<Row className="d-flex justify-content-center text-center">
 					<Col xs={12}>
 						<nav>
-							<ul className="d-flex">
-								<li><Link to=""></Link></li>
-								<li><Link to=""></Link></li>
-								<li><Link to=""></Link></li>
-								<li><Link to=""></Link></li>
+							<ul className="d-flex justify-content-center">
+								<li className="mx-3 px-4 py-2"><Link to="">Lorem.</Link></li>
+								<li className="mx-3 px-4 py-2"><Link to="">Lorem.</Link></li>
+								<li className="mx-3 px-4 py-2"><Link to="">Lorem.</Link></li>
+								<li className="mx-3 px-4 py-2"><Link to="">Lorem.</Link></li>
+								<li className="mx-3 px-4 py-2"><Link to="">Lorem.</Link></li>
+								<li className="mx-3 px-4 py-2"><Link to="">Lorem.</Link></li>
+								<li className="mx-3 px-4 py-2"><Link to="">Lorem.</Link></li>
 							</ul>
 						</nav>
 					</Col>
-					<Col xs={12}>
-						<h1>Explore the world’s leading design portfolios</h1>
+					<Col xs={6}>
+						<h1 className="mb-5">Explore the world’s leading design portfolios</h1>
 						<span>Millions of designers and agencies around the world showcase their portfolio work on Dribbble - the home to the world’s best design and creative professionals.</span>
 					</Col>
 					<Col xs={12} className="my-5">
